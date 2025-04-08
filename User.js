79dd5c0346aee706d605f23e659d51a7a1665f9a
ajith <154458588+ajithh1@users.noreply.github.com>
2025-04-08 10:08:12 +0530

    Update User.js

--- User.js
+++ User.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 
-const UserSchema=mongoose.Schema({
+const UserSchema= new mongoose.Schema({
     username:{type:String,require:true},
     email:{type:String,require:true},
     password:{type:String,require:true}
